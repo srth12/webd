085cf3be4cef6c37ac549b8c24e55bc56abfe39e
Sarath Babu P B <srth12@gmail.com>
2012-10-20 15:28:33 +0530

    my first commit

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Book360Deg.com</title>
+<?php
+if(isset($_REQUEST["login"])){
+$username=check_input($_REQUEST["username"]);
+$password=check_input($_REQUEST["password"]);
+echo $username;
+function check_input($value)
+{
+// Stripslashes
+if (get_magic_quotes_gpc())
+  {
+  $value = stripslashes($value);
+  }
+// Quote if not a number
+if (!is_numeric($value))
+  {
+  $value = "'" . mysql_real_escape_string($value) . "'";
+  }
+return $value;
+}
+
+}
+
+
+?>
+
+
+
+
+</head>
+<body>
+<!DOCTYPE html>
+<html>
+<body style="margin-top:-41px;">
+
+<div id="container" style="width:100%;height:550px;"><br />
+<div id="header" style="background-color:#FFA500;">
+<h1 style="margin-bbottom:0;">Book360deg: Book  Views and reviews</h1></div>
+<div id="Seach box" style="float:right">
+<form>
+Search: <input type="text" name="Searchbox">
+<input type="button" value="Go" /> 
+</form>
+</div>
+<div id="menu" style="background-color:#FFD700;height:100%;width:10%;float:left;">
+<b>Genre</b><br>
+Adventure<br>
+Comics<br>
+Science&Technology <br>
+Sci-Fi<br>
+Novel<br>
+Short Story<br>
+History<br>
+Autobiography
+</div>
+<div id="content" style="background-color:#EEEEEE;height:100%;width:70%;float:left;">
+Content goes here</div>
+<div id="login field" style="float:left">
+<form align="right" action="index.php" method="POST">
+User ID  : <input type="text" name="username"><br/>
+Password <input type="password" name="password"><br />
+<input type="submit" value="login" name="login" />
+</form>
+</div>
+</div>
+</form>
+</body>
+</html>
---
+++ insert.php
@@ -0,0 +1,80 @@
+<?php
+
+//include("auth.inc.php");
+if(isset($_REQUEST["submit"])){
+
+
+
+
+mysqli_connect("localhost","","") or die("could not connect to db stopping...");
+mysql_select_db("books") or die(" database selection failed");
+$img=$_FILES["image"];
+$image=$_FILES["image"]['name'];
+$source=$img['tmp_name'];
+$target="./images/".$image;
+
+//$img=$image["name"];
+//echo "hai".$_FILES["image"]["tmp_name"];
+$title=$_REQUEST['title'];
+$author=$_REQUEST['author'];
+$publisher=$_REQUEST['publisher'];
+$genre=$_REQUEST['genre'];
+$query1="insert into book_details values('$title','$author','$publisher','$genre','$image')";
+$query2="insert into book_index(title) values('$title') ";
+mysql_query($query1) or die("Inertion Failed 1");
+mysql_query($query2) or die(" insertion failed 2");
+
+
+if(copy($source,$target))
+{
+echo "file copied successfully";
+}
+else{
+die( "image uplaoding failed");
+}
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="css/css.css" >
+
+<title>Book 360deg</title>
+</head>
+<body>
+<img id="titlebar" src="./images/titlebar.jpg" width="100%" height="100px" />
+<form action="insert.php" method="post" enctype="multipart/form-data"  >
+<table>
+<tr>
+<td>Title</td>
+<td><input type="text" name="title" /></td>
+</tr>
+<tr>
+<td>Autor</td>
+<td><input type="text" name="author" /></td>
+</tr>
+<tr>
+<td>Publisher</td>
+<td><input type="text" name="publisher" /></td>
+</tr>
+<tr>
+<td>Genre</td>
+<td><input type="text" name="genre" /></td>
+</tr>
+<tr>
+<td> Upload Image</td>
+<td><input type="file" name="image"></td>
+</tr>
+<tr>
+<td colspan="2" >
+<input src="./images/titlebar.jpg" width="100px" height="10px" type="image"  value="submit" name="submit"></td>
+</table>
+
+
+</tr>
+
+
+</form>
+</body>
+</html>
