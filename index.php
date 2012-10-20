39a1bc8d5a6db5919bc4c74db9c50d9458cf0e58
Sarath Babu P B <srth12@gmail.com>
2012-10-20 15:56:12 +0530

    my second commit

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Book360Deg.com</title>
 <?php
-if(isset($_REQUEST["login"])){
-$username=check_input($_REQUEST["username"]);
-$password=check_input($_REQUEST["password"]);
-echo $username;
+
 function check_input($value)
 {
 // Stripslashes
@@ -22,11 +14,31 @@ if (!is_numeric($value))
   }
 return $value;
 }
+if(isset($_REQUEST["login"])){
+//echo $_REQUEST["username"];
+
+
+$username=check_input($_REQUEST["username"]);
+$password=check_input($_REQUEST["password"]);
+
+
+
+}
+if(isset($_REQUEST["go"])){
+$search=$_REQUEST["Searchbox"];
+$query="select from 
 
 }
 
 
 ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Book360Deg.com</title>
+
 
 
 
@@ -41,9 +53,9 @@ return $value;
 <div id="header" style="background-color:#FFA500;">
 <h1 style="margin-bbottom:0;">Book360deg: Book  Views and reviews</h1></div>
 <div id="Seach box" style="float:right">
-<form>
+<form action="index.php" method="POST" >
 Search: <input type="text" name="Searchbox">
-<input type="button" value="Go" /> 
+<input type="submit" value="Go" /> 
 </form>
 </div>
 <div id="menu" style="background-color:#FFD700;height:100%;width:10%;float:left;">
@@ -55,18 +67,21 @@ Sci-Fi<br>
 Novel<br>
 Short Story<br>
 History<br>
-Autobiography
+Autobiography<br/>
+Epic
 </div>
 <div id="content" style="background-color:#EEEEEE;height:100%;width:70%;float:left;">
 Content goes here</div>
 <div id="login field" style="float:left">
 <form align="right" action="index.php" method="POST">
-User ID  : <input type="text" name="username"><br/>
-Password <input type="password" name="password"><br />
-<input type="submit" value="login" name="login" />
+User ID  : 
+<input type="text" name="username"><br/>
+Password 
+<input type="password" name="password"><br />
+<input type="submit" value="login" name="login" >
 </form>
 </div>
 </div>
-</form>
+
 </body>
 </html>
